329596d4c3c76a9d446402140c34d6e086d917f6
Ines Rosito <ines.rosito@ucalgary.ca>
2019-02-28 12:27:34 -0700

    Missing Assignment 2 files

--- Assignment2/CoordinatesAJAX_Math.php
+++ Assignment2/CoordinatesAJAX_Math.php
@@ -1,52 +1,77 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="CoordinatesCSS.css">
-    <link href='https://fonts.googleapis.com/css?family=Didact Gothic' rel='stylesheet'>
-  </head>
-
-  <style>
-    body {font-family: 'Didact Gothic';font-size: 22px;}
-  </style>
-
-  <body>
-
-    <div id="background">
-        <img src="earth.gif" class="stretch" alt="" />
-    </div>
-
-    <div id="page-wrap">
-      <!--
-        // Creating form from which the user can input their coordinates
-        // ------------------------------------------------------------- //
-        // NOTE: action defined sends form data to same page, instead of
-        //       jumping to a different page -- Errors can be presented
-        //       on same page
-      -->
-
-      <h1><center> Assignment 2 </center></h1>
-      <h2><center> Task 1.4 </center></h2>
-
-      <center>Please input a ( <i>longitude, latitude</i> ) for the coordinates below <br><hr />
-      Longitude must be between [-90, 90] <br> Latitude must be between [-180, 180] <br><br></center>
-
-        <!--The actual form where the user inputs two sets of coordinates -->
-      <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-      <center><b>Coordinate 1: </b> <input type="text" name="Coordinate1"><br><br></center>
-      <center><b>Coordinate 2: </b> <input type="text" name="Coordinate2"><br><br></center>
-      <center><input type="submit" value="Submit"></center>
-      </form>
-
-
 <?php
-  // This particular if statement is unnecessary -- however, if you wish
-  // to implement GET request on the page, you can program different functionalities
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+// -------------------- FUNCTIONS -------------------- //
+
+function errorCheck($error){
+  if($error) {
+    foreach ($_POST as $key => $value) {
+      $form[$key] = htmlspecialchars($value);
+    }
+  }
+}
+
+function quadPrint($x, $y, $quad){
+  if (empty($quad))
+  {
+    echo "<center>($x, $y) is on the <b>Origin</b></center>";
+  }
+  else {
+    echo "<center>($x, $y) is in <b>Quadrant $quad</b></center>";
+  }
+}
+
+// Determining quadrant of coordinates
+function quadrant ($x, $y){
+  // Origin not confined to particular quadrant
+  if(($x == 0) AND ($y == 0)) {return 0;}
+  elseif(($x < 0) AND ($y < 0)) {return 3;}
+  elseif(($x < 0) AND ($y > 0)){return 2;}
+  elseif(($x > 0) AND ($y < 0)){return 4;}
+  elseif(($x > 0) AND ($y > 0)){return 1;}
+}
+
+// Calculating the bearing between the two points provided
+function bearing ($x1, $y1, $x2, $y2){
+
+  $x1Radians = deg2rad($x1);
+  $x2Radians = deg2Rad($x2);
+  $xDiffernceRadians = deg2Rad($x2-$x1);
+  $yDifferenceRadians = deg2Rad($y2-$y1);
+
+  $y = sin(deg2Rad($y2) - deg2Rad($y1)) * cos($x2Radians);
+  $x = cos($x1Radians) * sin($x2Radians) -
+        sin($x1Radians) * cos($x2Radians)*cos($yDifferenceRadians);
+  $bearing = round(atan2($y, $x), 3);
+  return $bearing;
+}
+
+// Calculating the great circle distance between the two points provided
+function haversine ($x1, $y1, $x2, $y2){
+  $xDiffernceRadians = deg2Rad($x2-$x1);
+  $yDifferenceRadians = deg2Rad($y2-$y1);
+  $x1Radians = deg2rad($x1);
+  $x2Radians = deg2Rad($x2);
+
+  $a =  sin($xDiffernceRadians/2) * sin($xDiffernceRadians/2) +
+        cos($x1Radians) * cos($x2Radians) *
+        sin($yDifferenceRadians/2) * sin($yDifferenceRadians/2);
+  $c = 2 * atan2(sqrt($a), sqrt(1-$a));
+  $d = 637100 * $c;
+  $d = round($d, 3);
+  return $c;
+}
+
+// Customizable Error Handler
+function customError( $errstr) {
+  echo "<br><center><b>Error:</b> $errstr<br></center>";
+  die();
+}
+
 
 // ===================== DATA FORM RETRIVAL  ===================== //
   // -------------------- SEPERATE LONG. & LAT. -------------------- //
-  $data1 = explode(",", $_POST["Coordinate1"]);
-  $data2 = explode(",", $_POST["Coordinate2"]);
+  $data1 = explode(",", $_GET['coord1']);
+  $data2 = explode(",", $_GET['coord2']);
 
 // ===================== ERROR CHECKING ===================== //
   // -------------------- TOO MANY ARGUMENTS? -------------------- //
@@ -112,7 +137,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $dist = bearing($coord1_x, $coord1_y, $coord2_x, $coord2_y);
     echo "<h3><center><b>-- Bearings --</b><center></h3>
           <center>The bearing between the two points is approximately:</center>
-          <center><b>$dist radians</b></center>";
+          <center><b>$dist</b></center>";
 
   // -------------------- CIRCLE DISTANCE? -------------------- //
 
@@ -120,64 +145,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "<h3><center><b>-- Great Circle Distance --</b><center></h3>
           <center>The great circle distance between the two points is
           approximately:</center>
-          <center><b>$dist meters</b></center><br>";
+          <center><b>$dist</b></center><br>";
     echo '</div>';
-}
-
-// -------------------- FUNCTIONS -------------------- //
-
-function errorCheck($error){
-  if($error) {
-    foreach ($_POST as $key => $value) {
-      $form[$key] = htmlspecialchars($value);
-    }
-  }
-}
-
-function quadPrint($x, $y, $quad){
-  if (empty($quad))
-  {
-    echo "<center>($x, $y) is on the <b>Origin</b></center>";
-  }
-  else {
-    echo "<center>($x, $y) is in <b>Quadrant $quad</b></center>";
-  }
-}
-
-  // Determining quadrant of coordinates
-function quadrant ($x, $y){
-    // Origin not confined to particular quadrant
-  if(($x == 0) AND ($y == 0)) {return 0;}
-  elseif(($x < 0) AND ($y < 0)) {return 3;}
-  elseif(($x < 0) AND ($y > 0)){return 2;}
-  elseif(($x > 0) AND ($y < 0)){return 4;}
-  elseif(($x > 0) AND ($y > 0)){return 1;}
-}
-
-  // Calculating the bearing between the two points provided
-function bearing ($x1, $y1, $x2, $y2){
-  $totalX = cos($y2)*sin($x2-$x1);
-  $totalY = cos($y1)*sin($y2)-sin($y1)*cos($y2)*cos(($x2-$x1));
-  $bearing = round(atan2($totalX, $totalY), 3);
-  return $bearing;
-}
-  // Calculating the great circle distance between the two points provided
-function haversine ($x1, $y1, $x2, $y2){
-  $dlon = $x2 - $x1;
-  $dlat = $y2 - $y1;
-
-  $a = pow(sin($dlat/2), 2) + cos($y1) * cos($y2) * pow(sin($dlon/2), 2);
-  $b = 2 * asin(min(1, sqrt($a)));
-  $c = round(6373*$b, 2);
-  return $c;
-}
-
-  // Customizable Error Handler
-function customError( $errstr) {
-  echo "<br><center><b>Error:</b> $errstr<br></center>";
-  die();
-}
 
 ?>
-</body>
-</html>
