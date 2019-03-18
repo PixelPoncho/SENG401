fb3dc91242fb03fb2641531f6c644a3a8b003bea
Ashley Brown <ashley.brown1@ucalgary.ca>
2019-03-18 17:42:20 -0600

    added assignment 4 start

---
+++ Assignment4.2/database/migrations/2019_03_14_195256_create_subscription_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscription', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('book_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+      //      $table->timestamp('timestamp');
+            $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscription');
+    }
+}
