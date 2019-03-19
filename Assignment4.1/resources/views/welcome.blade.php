eee691b9c9f490a0a77faf36e17769a8ab2069c3
Ashley Brown <ashley.brown1@ucalgary.ca>
2019-03-19 13:38:27 -0600

    started on books layout

--- Assignment4.1/resources/views/welcome.blade.php
+++ Assignment4.1/resources/views/welcome.blade.php
@@ -2,21 +2,13 @@
 
 @section('content')
 <div class="title m-b-md">
-    Laravel
+    SENG 401 - Assignment 4
 </div>
 
 <div class="links">
-<!--
-    <a href="https://laravel.com/docs">Docs</a>
-    <a href="https://laracasts.com">Laracasts</a>
-    <a href="https://laravel-news.com">News</a>
-    <a href="https://blog.laravel.com">Blog</a>
-    <a href="https://nova.laravel.com">Nova</a>
-    <a href="https://forge.laravel.com">Forge</a>
-    <a href="https://github.com/laravel/laravel">GitHub</a>
--->
-  <a href="/books">Books</a>
 
+  <a href="/books">Books</a>
+</div>
 @endsection
 <!--
 <!doctype html>
