c7a4cbfd8f67ed2d7d9b82b6a1333c4db4e41bf9
Ashley Brown <ashley.brown1@ucalgary.ca>
2019-03-19 13:00:19 -0600

    added framework for books page

---
+++ Assignment4.1/resources/views/books.blade.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE php>
+<html>
+  <head>
+
+  </head>
+  <body>
+    <p>
+      these are the books
+    </p>
+  </body>
+</html>
--- Assignment4.1/routes/web.php
+++ Assignment4.1/routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('books', function (){
+  return view('books');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
