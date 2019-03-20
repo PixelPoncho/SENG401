a8bd83d47bfa62d63f6a5aead71743c7e2f3d707
Nathan Godard <nathan.godard1@ucalgary.ca>
2019-03-19 22:14:51 -0600

    Added "add comment" form

--- Assignment4.1/resources/views/books.blade.php
+++ Assignment4.1/resources/views/books.blade.php
@@ -4,11 +4,12 @@
 <div class='container'>
   <div class='title'>
       <h1>Books</h1>
+
       <ul>
-      @foreach($books as $book)
-        <li><?=$book; ?></li>
-      @endforeach
-    </ul>
+        @foreach($books as $book)
+          <li>{{ $book }}</li>
+        @endforeach
+      </ul>
   </div>
 </div>
 @endsection
